e79dcda8391c18ee63360da389c9b3287d97efaf
Jan Gosmann <jan@hyper-world.de>
2011-05-03 15:47:25 +0200

    Some documentation.

--- src/asl/aslpreprocessor.h
+++ src/asl/aslpreprocessor.h
@@ -7,10 +7,16 @@
 namespace asl
 {
 
-class ASLPreprocessor
-{
-public:
-    ASLPreprocessor();
+/** GLSL preprocessor implementation tailored to the needs auf the \ref
+ * AnnotatedShaderCompiler. That means not all preprocessor directives will be
+ * translated. Nevertheless, at least the conditionals (\#if, \#ifdef, ...) and
+ * macros in these will work.
+ *
+ * \attention Be aware that you must not execute multiple calls to the methods
+ * of this class at the same time (e.g. from different threads). Even with
+ * different instances of this class you must not do this!
+ */
+class ASLPreprocessor { public: ASLPreprocessor();
 
     void process(const QString &sourcecode, QTextStream *out);
     QString process(const QString &sourcecode);
