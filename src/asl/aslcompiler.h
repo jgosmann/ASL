d11a1fcf27d4ebeb80e38f40ca57363c6f6f8bbe
Jan Gosmann <jan@hyper-world.de>
2011-05-18 11:14:32 +0200

    Documentation of AslCompiler.

--- src/asl/aslcompiler.h
+++ src/asl/aslcompiler.h
@@ -16,13 +16,29 @@ public:
     explicit ASLCompiler(QObject *parent = 0);
 
     /**
-     * Use always `/' as an universal directory separator for the
+     * Compiles and links an annotated shader program and returns a pointer to
+     * it.
+     *
+     * \note Use ASLCompiler#success() to test whether the last compilation was
+     * successful. Please note that ASLCompiler#log() may return a string
+     * containing warnings (but not errors) even if the compilation was
+     * successful.
+     *
+     * \note Use always `/' as an universal directory separator for the
      * \a pathOfSource argument.
+     *
+     * \attention You have to free the memory allocated for the shader when you
+     * do not need it anymore. Use \c delete for that.
      */
     asl::AnnotatedGLShaderProgram * compile(QGLShader::ShaderType type, const
             QString &source, const QString &pathOfSource = "");
 
     QString log() const { return m_log; }
+
+    /**
+     * \note Even if \c true is returned ASLCompiler#log() may return a string
+     * containing warnings (but not errors).
+     */
     const bool success() const { return m_success; }
 
 signals:
