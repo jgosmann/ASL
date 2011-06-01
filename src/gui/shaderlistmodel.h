198f09cb3be926ccefdef37d78dc668b0575c7cd
DenisLaptop <Denis.de.John@googlemail.com>
2011-06-01 11:35:19 +0200

    ShaderList als QAbstractListModel

--- src/gui/shaderlistmodel.h
+++ src/gui/shaderlistmodel.h
@@ -16,6 +16,8 @@ public:
     int rowCount( const QModelIndex & parent = QModelIndex() ) const;
     QVariant data( const QModelIndex & index, int role = Qt::DisplayRole ) const;
 
+    QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
+
     /**
       * Adds a shader to the List
       */
