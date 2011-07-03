6cd045e6fb604e720f66a2bb73103043a2da9570
Jan Gosmann <jan@hyper-world.de>
2011-07-03 21:07:47 +0200

    Fixing wrong return value of success() and log() if last shader was retrieved from the cache in CachedAnnotatedGLShaderCompier.

--- test/asl/cachedannotatedglshadercompilertest.cpp
+++ test/asl/cachedannotatedglshadercompilertest.cpp
@@ -13,6 +13,7 @@ public:
     CachedAnnotatedGLShaderCompilerTest() : stdType(QGLShader::Fragment)
     {
         ON_CALL(compilerMock, success()).WillByDefault(Return(true));
+        ON_CALL(compilerMock, log()).WillByDefault(Return(QString("")));
     }
 
     void setUp()
@@ -147,6 +148,28 @@ public:
         CPPUNIT_ASSERT(compiled1.data() != compiled2.data());
     }
 
+    void returningCachedShaderResetsSuccessAndLog()
+    {
+        EXPECT_CALL(compilerMock, compileFile(stdType, QString("succesful")))
+            .Times(1).WillOnce(Return(createDummyShader()));
+        const QString failingShader("failing");
+        Expectation failingShaderCompiled =
+            EXPECT_CALL(compilerMock, compileFile(stdType, failingShader))
+                .WillOnce(Return(createDummyShader()));
+
+        EXPECT_CALL(compilerMock, success()).WillRepeatedly(Return(true));
+        EXPECT_CALL(compilerMock, success()).Times(AtLeast(1)).After(failingShaderCompiled)
+            .WillRepeatedly(Return(false));
+
+        QSharedPointer<AnnotatedGLShader> compiled =
+                cachedCompiler->compileFile(stdType, "succesful");
+        cachedCompiler->compileFile(stdType, failingShader);
+        CPPUNIT_ASSERT(!cachedCompiler->success());
+        QSharedPointer<AnnotatedGLShader> compiled2 =
+                cachedCompiler->compileFile(stdType, "succesful");
+        CPPUNIT_ASSERT(cachedCompiler->success());
+    }
+
     CPPUNIT_TEST_SUITE(CachedAnnotatedGLShaderCompilerTest);
     CPPUNIT_TEST(compileFileCompilesShader);
     CPPUNIT_TEST(compileFileCachesCompiledShader);
@@ -155,6 +178,7 @@ public:
     CPPUNIT_TEST(compileFileAsMainCachesCompiledShader);
     CPPUNIT_TEST(doesNotCacheFailedAsMainCompilations);
     CPPUNIT_TEST(doesNotMixMainAndNonMainShadersInCache);
+    CPPUNIT_TEST(returningCachedShaderResetsSuccessAndLog);
     CPPUNIT_TEST_SUITE_END();
 
 private:
