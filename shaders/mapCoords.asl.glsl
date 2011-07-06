dff88d6321c4901c171208aec6545ed9392bed12
Jan Gosmann <jan@hyper-world.de>
2011-07-06 21:09:23 +0200

    Fix map coordinates.

--- shaders/mapCoords.asl.glsl
+++ shaders/mapCoords.asl.glsl
@@ -1,5 +1,3 @@
-#version 120
-
 /*
  * ShaderName: Map Coordinates
  * ShaderDescription: Functions for mapping coordinates between 0 and 1 (like
@@ -14,15 +12,17 @@ uniform int texHeight;
  * Default: 1
  * Range: 0.0001, max
  */
-/* uniform */ float zoomFactor = 1;
+/* uniform */ const float zoomFactor = 0.5;
 
 /*
  * Name: Translation
  * Default: vec2(0, 0)
  */
-/* uniform */ vec2 translation = vec2(0, 0);
+/* uniform */ const vec2 translation = vec2(0, 0);
 
 vec2 mapCoords(vec2 coords) {
-    return (2 * coords / vec2(texWidth, texHeight) - 1);
+    return (2.0 * coords / vec2(texWidth, texHeight) - 1.0
+             + vec2(-1, 1) * translation)
+         / zoomFactor;
 }
 
