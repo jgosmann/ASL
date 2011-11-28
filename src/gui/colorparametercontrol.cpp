56062fb71c137f035c02a5afc41c72cf0c3972ab
Jan Gosmann <jan@hyper-world.de>
2011-11-28 21:45:21 +0100

    Initialize color picker with default color. Addresses #5.

--- src/gui/colorparametercontrol.cpp
+++ src/gui/colorparametercontrol.cpp
@@ -13,6 +13,8 @@ ColorParameterControl::ColorParameterControl(asl::ShaderParameterInfo &info,
     QObject *listener)
   : m_info( info )
 {
+  m_colorArray = m_info.defaultValue.asFloat();
+
   QLabel *caption = new QLabel( info.name );
   QPushButton *pushButton = new QPushButton( "Select color ..." );
 
@@ -31,48 +33,42 @@ ColorParameterControl::ColorParameterControl(asl::ShaderParameterInfo &info,
 
 ColorParameterControl::~ColorParameterControl()
 {
-  delete [] m_colorArray;
 }
 
 
 void ColorParameterControl::showColorDialog()
 {
   QColor color;
-
-  const GLfloat *defaultColorArray = m_info.defaultValue.asFloat();
-
   QColor defaultColor = QColor();
 
   if( m_info.type->rowDimensionality() == 3 ) // vec3
   {
-    defaultColor.setRgbF( defaultColorArray[0], defaultColorArray[1], 
-        defaultColorArray[2] );
+    defaultColor.setRgbF( m_colorArray[0], m_colorArray[1], 
+        m_colorArray[2] );
 
     color = QColorDialog::getColor( defaultColor, &m_widget, 
         "Please select a color ..." );
 
-    m_colorArray = new GLfloat[3];
-    m_colorArray[0] = color.redF(); 
-    m_colorArray[1] = color.greenF(); 
-    m_colorArray[2] = color.blueF(); 
   }
   else // vec4
   {
-    defaultColor.setRgbF( defaultColorArray[0], defaultColorArray[1], 
-        defaultColorArray[2], defaultColorArray[3] );
+    defaultColor.setRgbF( m_colorArray[0], m_colorArray[1], 
+        m_colorArray[2], m_colorArray[3] );
 
     color = QColorDialog::getColor( defaultColor, &m_widget, 
         "Please select a color ...",
         QColorDialog::ShowAlphaChannel );
 
-    m_colorArray = new GLfloat[4];
-    m_colorArray [0] = color.redF();
-    m_colorArray [1] = color.greenF();
-    m_colorArray [2] = color.blueF();
     m_colorArray [3] = color.alphaF();
   }
 
-  emit colorChanged();
+  m_colorArray [0] = color.redF();
+  m_colorArray [1] = color.greenF();
+  m_colorArray [2] = color.blueF();
+
+  if (color != defaultColor) {
+    emit colorChanged();
+  }
 }
 
 
